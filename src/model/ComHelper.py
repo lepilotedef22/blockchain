f0b987ae4334521a209bf54cd3f4448d793f1ff5
lepilotedef22 <lepilotedef22@gmail.com>
2018-12-04 15:24:30 +0100

    config_parser working, tested cf ComHelpertest.py

--- src/model/ComHelper.py
+++ src/model/ComHelper.py
@@ -9,7 +9,18 @@ This module contains the helping functions used to simplify the communication cl
 
 # ----------------------------------------------------- IMPORTS ----------------------------------------------------- #
 
+# For types
+
+from typing import Dict
+
+# For formatting
+
 from sys import byteorder
+from configparser import ConfigParser
+
+# For file explorer
+
+from pathlib import Path
 
 # ---------------------------------------------------- FUNCTIONS ---------------------------------------------------- #
 
@@ -26,3 +37,30 @@ def format_message(msg: str, num_bytes: int) -> bytes:
     msg_length = len(msg) + num_bytes
     length_in_bytes = msg_length.to_bytes(num_bytes, byteorder)
     return length_in_bytes + str.encode(msg)
+
+
+def parse_config(index: int) -> Dict:
+
+    """
+    Parses the .ini conf file related to the node at index. The conf file has the structure :
+        {node : {ip_address, user_name}, registration : {authenticate_ip, secret}, neighbours : [neighbour_ip]}
+    :param index: index of the node
+    :return: a dictionary of the data in the conf file
+    """
+
+    # Finding the path to config file
+
+    file_name = "host_{}.ini".format(index)
+    blockchain_path = Path.cwd().parent
+    conf_path = blockchain_path / "config" / file_name
+
+    # Opening and reading the file
+
+    parser = ConfigParser(allow_no_value=True)
+    parser.read(conf_path)
+
+    # formatting the result in the correct way
+
+    return {'node': dict(parser.items('node')),
+            'registration': dict(parser.items('registration')),
+            'neighbours': [elem[0] for elem in parser.items('neighbours')]}
--- tests/ComHelperTest.py
+++ tests/ComHelperTest.py
@@ -1,24 +1,38 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-This unit test class is used to test the communication modules of the project
-"""
-
-__author__ = "Denis Verstraeten"
 __date__ = "29/11/2018"
 
 # ----------------------------------------------------- IMPORTS ----------------------------------------------------- #
 
-import unittest
-
-from src.model.RX import RX
-from src.model.TX import TX
+from unittest import TestCase, main
+from src import parse_config
 
 # ------------------------------------------------------ TESTS ------------------------------------------------------ #
 
 
+class ComHelperTest(TestCase):
+
+    """
+    This test file assesses the ComHelperModule
+    """
+
+    def test_config_parser(self):
+
+        """
+        Tests the config_parser function
+        """
+
+        expected_dico = {
+            'node': {'ip_address': '127.0.0.1', 'username': 'host_1'},
+            'registration': {'ip_address': '127.0.0.10', 'secret': 'host_1_secret'},
+            'neighbours': ['127.0.0.2', '127.0.0.3']
+        }
+
+        self.assertEqual(parse_config(1), expected_dico)
+
 #  -----------------------------------------------------LAUNCHER ----------------------------------------------------- #
 
+
 if __name__ == '__main__':
-    unittest.main()
+    main()
