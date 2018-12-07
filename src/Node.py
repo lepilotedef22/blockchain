1f7c170347e7f98b9b4360d4f19904fe5c342f3a
lepilotedef22 <lepilotedef22@gmail.com>
2018-12-07 22:29:39 +0100

    new communication implementation started, Node updated

--- src/Node.py
+++ src/Node.py
@@ -1,12 +1,47 @@
-from src import TX
+# !/usr/bin/env python3
+# -*- coding: utf-! -*-
+
+# ----------------------------------------------------- IMPORTS ----------------------------------------------------- #
+
 from threading import Thread
+from src import parse_config_node
+from socket import socket
+
+
+__date__ = "07.12.2018"
+
 
 class Node(Thread):
-    def __init__(self):
-        super().__init__()
-        self.nodeTX = TX("localhost", 4243)
-        #TODO
-        print("New node")
 
-    def run(self) -> None:
-        self.nodeTX.send("localhost", 4242, "Hello world")
+    """
+    Class representing a node of the network.
+    """
+
+    # ------------------------------------------------- CONSTRUCTOR ------------------------------------------------- #
+
+    def __init__(self,
+                 node_idx: int
+                 ) -> None:
+
+        """
+        Constructor of the Node class
+        :param node_idx: index of the node used to find the config file to parse
+        """
+
+        super().__init__()
+
+        # Parsing config files
+        # TODO : update network topology according to assignment
+        config = parse_config_node(node_idx)
+        self.username = config['node']['username']
+        self.ip = config['node']['ip_address']
+        self.authenticate_ip = config['registration']['ip_address']
+        self.secret = config['registration']['secret']
+        self.neighbours_ip = config['neighbours']
+
+        self.port = 5001  # Arbitrary, given in the assignment
+        self.connected_neighbours = {}  # Storing the connected neighbours to allow the communication: {ip: socket}
+
+
+    # --------------------------------------------------- METHODS --------------------------------------------------- #
+
