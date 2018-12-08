32bb249e9b0c497fc19e082d6dd7e76e528f9726
lepilotedef22 <lepilotedef22@gmail.com>
2018-12-08 23:30:22 +0100

    Node run method created

--- src/Node.py
+++ src/Node.py
@@ -4,7 +4,7 @@
 # ----------------------------------------------------- IMPORTS ----------------------------------------------------- #
 
 from threading import Thread
-from src import parse_config_node
+from src import parse_config_node, Blockchain
 from socket import socket
 
 
@@ -34,14 +34,41 @@ class Node(Thread):
         # TODO : update network topology according to assignment
         config = parse_config_node(node_idx)
         self.username = config['node']['username']
-        self.ip = config['node']['ip_address']
+        self.ip = config['node']['ip_address']  # Alias loopback address (cf. assignment)
         self.authenticate_ip = config['registration']['ip_address']
         self.secret = config['registration']['secret']
         self.neighbours_ip = config['neighbours']
 
         self.port = 5001  # Arbitrary, given in the assignment
         self.connected_neighbours = {}  # Storing the connected neighbours to allow the communication: {ip: socket}
-
+        self.blockchain = Blockchain()
+        self.authenticated = False  # Authenticated to the network ?
 
     # --------------------------------------------------- METHODS --------------------------------------------------- #
 
+    def __authenticate(self, socket: socket) -> None:
+
+        """
+        Authenticates the node to the authentication center. Once the node is authenticated, its authenticated attribute
+        is switched to True
+        :param socket: the socket used to communicate with the authentication center
+        """
+
+    # ----------------------------------------------------- RUN ----------------------------------------------------- #
+
+    def run(self) -> None:
+
+        """
+        Main method of the Node thread :
+            1. tries to be authenticated on the network by the authentication center
+        """
+
+        with socket() as sock:
+
+            # Creating as socket with default mode : IPv4 and TCP
+            sock.bind((self.ip, self.port))
+
+            while not self.authenticated:
+
+                # Trying to be authenticated on the network
+                self.__authenticate(sock)
---
+++ src/model/bitcop/BitcopMessage.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------- IMPORTS ----------------------------------------------------- #
+
+
+# TODO receive data, deduce length, add static constants for the type of communication, overload __byte__, __str__
