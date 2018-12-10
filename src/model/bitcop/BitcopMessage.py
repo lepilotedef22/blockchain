b3f3d8f5f7719290c4ce58add9a0124fdf634e7d
Van Heirstraeten Arthur <avheirst@ulb.ac.be>
2018-12-10 22:21:40 +0100

    Revert "Node run method created"
    This reverts commit 32bb249e9b0c497fc19e082d6dd7e76e528f9726.

--- src/model/bitcop/BitcopMessage.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# ----------------------------------------------------- IMPORTS ----------------------------------------------------- #
-
-
-# TODO receive data, deduce length, add static constants for the type of communication, overload __byte__, __str__
